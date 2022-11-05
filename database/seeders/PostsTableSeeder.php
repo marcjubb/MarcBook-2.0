38e69e8f77c1b406361407a24c5627966efc2c4f
marcjubb <marc.jubb@live.co.uk>
2022-11-05 17:48:33 +0000

    Seeders

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*$u1 = new Post();
+        $u1 -> name = "Jason";
+        $u1 -> username = "Jason.gormo";
+        $u1 -> email = "jason@gmail.com";
+        $u1 -> password = "FlyAway";
+        $u1 -> save();*/
+        Post::factory()-> count(10) ->create();
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*$u1 = new User();
+        $u1 -> name = "Jason";
+        $u1 -> username = "Jason.gormo";
+        $u1 -> email = "jason@gmail.com";
+        $u1 -> password = "FlyAway";
+        $u1 -> save();*/
+       User::factory()-> count(10) ->create();
+
+    }
+}
