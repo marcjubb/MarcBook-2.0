60db7c4a5c6562cc3e3ddf3afb94fbbe41e6c8b6
marcjubb <marc.jubb@live.co.uk>
2022-11-14 16:50:54 +0000

    Register Function added

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,16 +8,16 @@ class RegisterController extends Controller
 {
     public function create()
     {
-        return view('auth.register');
+        return view('create_user');
     }
 
     public function store()
     {
         $attributes = request()->validate([
-            'name' => 'required|max:255',
-            'username' => 'required|min:3|max:255|unique:users,username',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:7|max:255',
+            'name' => ['required','max:255'],
+            'username' => ['required','min:4','max:255','unique:users,username'],
+            'email' => ['required','email','max:255','unique:users','email'],
+            'password' => ['required','min:4','max:255']
         ]);
 
         auth()->login(User::create($attributes));
